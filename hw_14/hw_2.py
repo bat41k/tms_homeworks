eba2598c3fff48ed9ec5054af5f9931760d544db
bat41k <bat41k@mail.ru>
2023-01-08 19:59:41 +0300

    Corrected hw_14

--- hw_14/hw_2.py
+++ hw_14/hw_2.py
@@ -3,13 +3,16 @@ import sqlite3
 with sqlite3.connect('my_bd.db') as con:
     cur = con.cursor()
 
-    cur.execute('''CREATE TABLE category (
-    category_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    category_title TEXT
-    )''')
+    cur.execute('''
+        CREATE TABLE category (
+        category_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        category_title TEXT
+        )
+        ''')
 
-    cur.execute('''INSERT INTO category VALUES
-    (1, 'Sports'),
-    (2, 'Fashion'),
-    (3, 'Food')
-    ''')
+    cur.execute('''
+        INSERT INTO category VALUES
+        (1, 'Sports'),
+        (2, 'Fashion'),
+        (3, 'Food')
+        ''')
